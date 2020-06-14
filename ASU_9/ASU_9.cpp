3011144dcf133f97465f83256c3a14fac361359f
Winter4 <Crowl1ngNice@yandex.ru>
2020-06-14 18:06:53 +0200

    9. Commented

--- ASU_9/ASU_9.cpp
+++ ASU_9/ASU_9.cpp
@@ -1,24 +1,25 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
-struct term {
-	int rusLang;
-	int math;
-	int coding;
-	int phyl;
+struct term { // структура с оценками студента
+	int rusLang; // русский
+	int math; // матан
+	int coding; // програмач
+	int phyl; // философия
 };
 
-
-struct student {
-	char surname[10];
-	char name[10];
-	char country[10];
-	char faculty[7];
-	char group[7];
-	term terms[2];
+struct student { // структура с "досье" студента
+	char surname[10]; // фамилия
+	char name[10]; // имя
+	char country[10]; // страна
+	char faculty[7]; // факультет 
+	char group[7]; // группа
+	term terms[2]; // массив структур с оценками за 2 семестра
 };
 
-void print_struct(student studs[20], int n)
+// теперь student - это имя ТИПА, как и term
+
+void print_struct(student studs[20], int n) // печать структур, хранящихся в памяти компа
 {
 	printf("№    Фамилия  Имя     Страна  Факультет  Группа  Сем. 1  Рус. яз.  Мат.  Пр.  Фил.  Сем. 2  Рус. яз.  Мат.  Пр.  Фил. \n");
 	for (int i = 0; i < n; i++) {
@@ -40,43 +41,39 @@ void print_struct(student studs[20], int n)
 	}
 }
 
-void read_file(student* studs, int* n)
+void read_file(student* studs, int* n) // чтение данных из файла
 {
-	FILE* file;
-
+	FILE* file; // указатель на файл
+	 
 	do {
 		printf("\n Введите имя файла для считывания: ");
 		char name[10];
 		scanf("%s", &name);
 
 		file = fopen(name, "r");
-	} while (file == NULL);
+	} while (file == NULL); // цикл будет повторяться, пока не откроется файл 
 
-	while (!(feof(file)) && *n < 20) { // 20 - размер массива
-		fscanf(file, "%s%s%s%s%s%d%d%d%d%d%d%d%d", &studs[*n].surname, &studs[*n].name, &studs[*n].country, &studs[*n].faculty,
+	// когда мы открыли файл, в нём появился некий внутренний указатель, который выбирает текущий элемент чтобы с ним работать
+	// по умолчанию указатель в самом начале стоит
+	while (!(feof(file)) && *n < 20) { // feof возвращает true, если достигнут конец файла; не двигает указатель         20 - размер ст. массива
+		// fscanf считывает данные из файла и ДВИГАЕТ указатель вперёд
+		fscanf(file, "%s%s%s%s%s%d%d%d%d%d%d%d%d", &studs[*n].surname, &studs[*n].name, &studs[*n].country, &studs[*n].faculty, 
 			&studs[*n].group, &studs[*n].terms[0].rusLang, &studs[*n].terms[0].math, &studs[*n].terms[0].coding, &studs[*n].terms[0].phyl,
 			&studs[*n].terms[1].rusLang, &studs[*n].terms[1].math, &studs[*n].terms[1].coding, &studs[*n].terms[1].phyl);
-		(*n)++;
+		(*n)++; // количество считанных структур
 	}
 
-	fclose(file);
+	fclose(file); // закрыли поток (файл короче)
 	print_struct(studs, *n);
 }
 
-//struct term {
-//	int rusLang;
-//	int math;
-//	int coding;
-//	int phyl;
-//};
-
-student add_struct()
+student add_struct() // функция типа структуры, которая вводит в память структуру с клавиатуры
 {
-	student newStudent = {};
+	student newStudent = {}; // описали структуру типа студент
 
 	printf("\n");
 
-	do {
+	do { // тут и ниже вроде всё стандартно
 		printf("Фамилия: ");
 		scanf("%s", &newStudent.surname);
 	} while (!(strlen(newStudent.surname) > 0));
@@ -145,14 +142,14 @@ student add_struct()
 		scanf("%d", &newStudent.terms[1].phyl);
 	} while (!(newStudent.terms[1].phyl > 0 && newStudent.terms[1].phyl <= 5));
 
-	return newStudent;
+	return newStudent; // т.к. функа типа структуры, она возвращает структуру. в мейне мы пишем структура = add_struct()
 }
 
-void edit_struct(student* studs, int n)
+void edit_struct(student* studs, int n) // редактирование структуры лапками
 {
 	printf("\n Какую запись редактировать: ");
 	int str;
-	scanf("%d", &str);
+	scanf("%d", &str); // я мб забыл тут проверку сделать, не вводим ли мы номр структуры > n, не помню уже
 
 	do {
 		printf("Сем. 2  Русский яз.: ");
@@ -177,7 +174,7 @@ void edit_struct(student* studs, int n)
 	print_struct(studs, n);
 }
 
-void save_structs(student studs[20], int n)
+void save_structs(student studs[20], int n) // сохраняем структуру в файл
 {
 	FILE* file;
 
@@ -189,8 +186,8 @@ void save_structs(student studs[20], int n)
 		file = fopen(name, "w");
 	} while (file == NULL);
 
-	for (int i = 0; i < n; i++)
-		fprintf (file, "%-3d  %-8s %-4s %-7s %-10s %-6s  %12d %7d %5d %5d %14d %6d %5d %5d \n", 
+	for (int i = 0; i < n; i++)  // фпринтф пишет в файл то, что мы скажем
+		fprintf (file, "%-3d  %-8s %-4s %-7s %-10s %-6s  %12d %7d %5d %5d %14d %6d %5d %5d \n",  // работает как и обычный принтф, только в файле
 			i + 1,
 			studs[i].surname,
 			studs[i].name,
@@ -212,10 +209,11 @@ int main()
 {
 	setlocale(LC_ALL, "russian");
 
-	student students[20] = {};
-	int n = 0;
+	student students[20] = {}; // основной массив структур
+	int n = 0; // количество структур (потом улетит в функу для приобретения значения)
 
-	do {
+	do { // основной цикл, который по сути и создаёт то самое меню
+		 // в конце у него true, и закончится он только когда ch = 0
 		printf("\n 1 - Считать данные из файла \n 2 - Редактировать оценки за 2й семестр \n 3 - Добавить студента \n 4 - Сохранить данные \n 0 - Выход \n");
 		int ch;
 		scanf("%d", &ch);
